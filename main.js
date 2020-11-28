45e85af57fa7caf5d3607f7c7191f38090da6018
Adebisi Ahmed <adebisi.ahmed10@gmail.com>
2020-11-28 11:09:52 +0100

    fixes

--- main.js
+++ main.js
@@ -175,10 +175,8 @@ const getUserProfileTemplate = ({ avatarUrl, name, bio, twitterUsername }) => {
         </H1>
         </div>
           <button class="btn btn-sm btn-status mobile-status" type="submit" value="Star" aria-label="Star this repository">
-            <svg class="octicon octicon-smiley" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true">
-              <path fill-rule="evenodd" d="M1.5 8a6.5 6.5 0 1113 0 6.5 6.5 0 01-13 0zM8 0a8 8 0 100 16A8 8 0 008 0zM5 8a1 1 0 100-2 1 1 0 000 2zm7-1a1 1 0 11-2 0 1 1 0 012 0zM5.32 9.636a.75.75 0 011.038.175l.007.009c.103.118.22.222.35.31.264.178.683.37 1.285.37.602 0 1.02-.192 1.285-.371.13-.088.247-.192.35-.31l.007-.008a.75.75 0 111.222.87l-.614-.431c.614.43.614.431.613.431v.001l-.001.002-.002.003-.005.007-.014.019a1.984 1.984 0 01-.184.213c-.16.166-.338.316-.53.445-.63.418-1.37.638-2.127.629-.946 0-1.652-.308-2.126-.63a3.32 3.32 0 01-.715-.657l-.014-.02-.005-.006-.002-.003v-.002h-.001l.613-.432-.614.43a.75.75 0 01.183-1.044h.001z"></path>
-            </svg>
-            <span class="btn-status-text">Set Status</span>
+            <g-emoji class="g-emoji" alias="house" fallback-src="https://github.githubassets.com/images/icons/emoji/unicode/1f3e0.png">🏠</g-emoji>
+            <span class="btn-status-text">Working from home</span>
           </button>
         <div class="profile-description">
           ${bio || ""}
@@ -188,7 +186,7 @@ const getUserProfileTemplate = ({ avatarUrl, name, bio, twitterUsername }) => {
 const getHeaderTemplate = ({ avatarUrl }) => {
   return `<header class="header">
   <button class="btn-link mobile-hamburger" type="button" aria-label="Toggle navigation" aria-expanded="false">
-    <svg height="24" class="octicon octicon-three-bars" viewBox="0 0 16 16" version="1.1" width="24" aria-hidden="true">
+    <svg height="22" class="octicon octicon-three-bars" viewBox="0 0 16 16" version="1.1" width="24" aria-hidden="true">
       <path fill-rule="evenodd" d="M1 2.75A.75.75 0 011.75 2h12.5a.75.75 0 110 1.5H1.75A.75.75 0 011 2.75zm0 5A.75.75 0 011.75 7h12.5a.75.75 0 110 1.5H1.75A.75.75 0 011 7.75zM1.75 12a.75.75 0 100 1.5h12.5a.75.75 0 100-1.5H1.75z"></path>
     </svg>
   </button>
